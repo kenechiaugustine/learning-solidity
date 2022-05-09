afbf0c713371c4eeb9453c5daa64aa95003656a4
Kene Arionye <kenechiaugustine@gmail.com>
2022-05-09 19:10:09 +0100

    Importing Library

--- contract.sol
+++ contract.sol
@@ -1,12 +1,5 @@
 pragma solidity 0.8.13;
-
-library Math {
-    function divide(uint256 a, uint256 b) internal pure returns (uint256) {
-        require(b > 0);
-        uint256 c = a / b;
-        return c;
-    }
-}
+import "MathLib.sol";
 
 contract MyContract {
 
