1f2d14c7da6552905538d243482b94d62f31ab4c
Kene Arionye <kenechiaugustine@gmail.com>
2022-07-23 12:13:13 +0100

    created contract constructor and global vars

--- Fundraiser.sol
+++ Fundraiser.sol
@@ -1,4 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// raising 
+contract Fundraiser {
+   uint256 public targetAmount;
+   address public owner;
+   mapping(address => uint256) public donations;
+
+   uint256 public raisedAmount = 0;
+   uint256 public finishTime = block.timestamp + 2 weeks;
+
+   constructor (uint256 _targetAmount) {
+        targetAmount = _targetAmount;
+        owner = msg.sender;
+   }
+}
